dde40936e47e80ee75f218b569f74c3c74f06985
reiend <loncerasjakerei@gmail.com>
2024-07-25 06:05:20 +0800

    audio support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -47,10 +47,19 @@
   # Enable sound.
   # hardware.pulseaudio.enable = true;
   # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+
+  # Remove sound.enable or set it to false if you had it set previously, as sound.enable is only meant for ALSA-based configurations
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+  };
+
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
