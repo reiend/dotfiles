8548e800b8fd1e1e5912b2f6e1bb982335532e59
reiend <loncerasjakerei@gmail.com>
2024-10-10 03:52:41 +0000

    untracked nixos derivation output grouped wsl and laptop derivation output

--- system/nixos/default.nix
+++ system/nixos/default.nix
@@ -3,13 +3,13 @@ let
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 {
-  rebuildWSL = pkgs.writeShellScriptBin "rebuild_wsl" ''
-    cp /etc/nixos/hardware-configuration.nix ./src/hosts/wsl/hardware-generated.nix
-    sudo nixos-rebuild switch -I nixos-config=./src/hosts/wsl
-  '';
-
-  rebuildLaptop = pkgs.writeShellScriptBin "rebuild_laptop" ''
-    cp /etc/nixos/hardware-configuration.nix ./src/hosts/laptop/hardware-generated.nix
-    sudo nixos-rebuild switch -I nixos-config=./src/hosts/laptop
-  '';
+  rebuild = {
+    wsl = pkgs.writeShellScriptBin "/rebuild/wsl" ''
+      sudo nixos-rebuild switch -I nixos-config=./src/hosts/wsl
+    '';
+    laptop = pkgs.writeShellScriptBin "rebuild/laptop" ''
+      sudo cp /etc/nixos/hardware-configuration.nix ./src/hosts/laptop/hardware-generated.nix
+      sudo nixos-rebuild switch -I nixos-config=./src/hosts/laptop
+    '';
+  };
 }
