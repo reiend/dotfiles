11afbcf9d4e9df29e003585f20da2066c92c142b
reiend <loncerasjakerei@gmail.com>
2024-10-10 16:46:12 +0800

    uncomment current windows setup

--- system/windows/script.ps1
+++ system/windows/script.ps1
@@ -1,32 +1,30 @@
+. "$($PSScriptRoot)\scoop\install"
 
-#. "$($PSScriptRoot)\scoop\install"
-#
-#. "$($PSScriptRoot)\git\install"
-#
-#. "$($PSScriptRoot)\fzf\install"
-#
-#. "$($PSScriptRoot)\nodejs\install"
-#
-#. "$($PSScriptRoot)\llvm\install"
-#
-#. "$($PSScriptRoot)\mingw\install"
-#
-#. "$($PSScriptRoot)\ripgrep\install"
-#
-#. "$($PSScriptRoot)\neovim\install"
-#. "$($PSScriptRoot)\neovim\set"
-#
-#. "$($PSScriptRoot)\powershell\install"
-#. "$($PSScriptRoot)\powershell\set"
-#
-#. "$($PSScriptRoot)\windows-terminal\install"
-#. "$($PSScriptRoot)\windows-terminal\set"
-#
-#. "$($PSScriptRoot)\terminal-icons\install"
+. "$($PSScriptRoot)\git\install"
+
+. "$($PSScriptRoot)\fzf\install"
+
+. "$($PSScriptRoot)\nodejs\install"
+
+. "$($PSScriptRoot)\llvm\install"
+
+. "$($PSScriptRoot)\mingw\install"
+
+. "$($PSScriptRoot)\ripgrep\install"
+
+. "$($PSScriptRoot)\neovim\install"
+. "$($PSScriptRoot)\neovim\set"
+
+. "$($PSScriptRoot)\powershell\install"
+. "$($PSScriptRoot)\powershell\set"
+
+. "$($PSScriptRoot)\windows-terminal\install"
+. "$($PSScriptRoot)\windows-terminal\set"
+
+. "$($PSScriptRoot)\terminal-icons\install"
 
 # build tools with editor
 . "$($PSScriptRoot)\visual-studio-community\install"
 
 # build tools only
 #. "$($PSScriptRoot)\visual-studio-build-tools\install"
-
