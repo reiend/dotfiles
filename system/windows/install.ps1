e0edd26ae4882c1ed5bbe43a82af0045b2f9b209
reiend <reiend.dev@gmail.com>
2026-02-14 03:25:35 +0800

    feat: added system installation via powershell or pwsh

---
+++ system/windows/install.ps1
@@ -0,0 +1,23 @@
+# PACKAGE MANAGER
+. "$($PSScriptRoot)\app\scoop.ps1"
+
+# FONTS
+. "$($PSScriptRoot)\font\iosevka-nf.ps1"
+
+# TERMINAL APPS
+. "$($PSScriptRoot)\app\neovim.ps1"
+. "$($PSScriptRoot)\app\glaze-wm.ps1"
+. "$($PSScriptRoot)\app\terminal-icons.ps1"
+. "$($PSScriptRoot)\app\pwsh.ps1"
+. "$($PSScriptRoot)\app\windows-terminal.ps1"
+. "$($PSScriptRoot)\app\ripgrep.ps1"
+. "$($PSScriptRoot)\app\llvm.ps1"
+. "$($PSScriptRoot)\app\openjdk.ps1"
+. "$($PSScriptRoot)\app\python.ps1"
+. "$($PSScriptRoot)\app\mingw.ps1"
+. "$($PSScriptRoot)\app\tree-sitter.ps1"
+
+# GUI APPS
+. "$($PSScriptRoot)\app\blender.ps1"
+. "$($PSScriptRoot)\app\musescore.ps1"
+. "$($PSScriptRoot)\app\audacity.ps1"
