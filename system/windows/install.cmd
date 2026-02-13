e115e3a065e0c4258530b6a3e9c4514b403da98b
reiend <reiend.dev@gmail.com>
2026-02-13 17:40:35 +0800

    fix: installation setup not working on parent directories

--- system/windows/install.cmd
+++ system/windows/install.cmd
@@ -1,23 +1,25 @@
 @ECHO off
 
+set "INSTALL_PATH=%~dp0"
+
 REM PACKAGE MANAGER
-powershell -File ".\app\scoop.ps1"
+powershell -File "%INSTALL_PATH%app\scoop.ps1"
 
 REM FONTS
-powershell -file ".\font\iosevka-nf.ps1"
+powershell -file "%INSTALL_PATH%font\iosevka-nf.ps1"
 
 REM TERMINAL APPS
-powershell -File ".\app\neovim.ps1"
-powershell -file ".\app\glaze-wm.ps1"
-powershell -file ".\app\terminal-icons.ps1"
-powershell -file ".\app\pwsh.ps1"
-powershell -file ".\app\windows-terminal.ps1"
-powershell -file ".\app\ripgrep.ps1"
+powershell -File "%INSTALL_PATH%app\neovim.ps1"
+powershell -file "%INSTALL_PATH%app\glaze-wm.ps1"
+powershell -file "%INSTALL_PATH%app\terminal-icons.ps1"
+powershell -file "%INSTALL_PATH%app\pwsh.ps1"
+powershell -file "%INSTALL_PATH%app\windows-terminal.ps1"
+powershell -file "%INSTALL_PATH%app\ripgrep.ps1"
 
 REM GUI APPS
-powershell -file ".\app\blender.ps1"
-powershell -file ".\app\musescore.ps1"
-powershell -file ".\app\lmms.ps1"
-powershell -file ".\app\audacity.ps1"
+powershell -file "%INSTALL_PATH%app\blender.ps1"
+powershell -file "%INSTALL_PATH%app\musescore.ps1"
+powershell -file "%INSTALL_PATH%app\lmms.ps1"
+powershell -file "%INSTALL_PATH%app\audacity.ps1"
 
 pause
