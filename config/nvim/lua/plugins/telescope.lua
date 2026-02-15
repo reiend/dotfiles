1e74d22893767891645ebf1e756412bd620f8e83
reiend <reiend.dev@gmail.com>
2026-02-15 12:12:01 +0800

    feat: telescope for git status

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -10,5 +10,6 @@ return {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
   end,
 }
