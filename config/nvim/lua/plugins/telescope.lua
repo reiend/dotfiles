9275622edc0b0e850fc286cc61d9c811e9f93116
reiend <reiend.dev@gmail.com>
2026-02-18 12:06:04 +0800

    feat: updated keymap telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,10 +6,10 @@ return {
     require('telescope').setup()
     local builtin = require 'telescope.builtin'
 
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
   end,
 }
