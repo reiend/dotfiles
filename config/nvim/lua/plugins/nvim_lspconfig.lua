de033ab5f049490e4445505f3957424794eb85ff
reiend <reiend.dev@gmail.com>
2026-02-22 18:11:06 +0800

    fix: added support for multiple java runtime for nvim-jdtls

--- config/nvim/lua/plugins/nvim_lspconfig.lua
+++ config/nvim/lua/plugins/nvim_lspconfig.lua
@@ -46,6 +46,10 @@ return {
 
     local blink_lsp_capabilities = require('blink.cmp').get_lsp_capabilities()
     local servers = {}
+    local is_win = function()
+      local os = string.lower(vim.uv.os_uname().sysname)
+      return string.find(os, 'win')
+    end
 
     local setup_server = function(name)
       local capabilities = vim.tbl_deep_extend(
@@ -60,53 +64,20 @@ return {
       }
 
       if name == 'jdtls' then
+        local nvim_jdtls_config = {}
         local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-
         local root_dir = vim.fs.root(
           0,
           { 'gradlew', '.git', 'mvnw', 'build.gradle', 'pom.xml' }
         ) or vim.fn.getcwd()
 
-        local bundles = {
-          vim.fn.expand(
-            vim.fn.stdpath 'data'
-              .. '/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar'
-          ),
-        }
-
-        local get_equinox_launcher_path = function()
-          return vim.fn.expand(
-            vim.fn.stdpath 'data'
-              .. '/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'
-          )
-        end
-
-        local get_config_path = function()
-          local os = string.lower(vim.uv.os_uname().sysname)
-
-          if string.find(os, 'win') then
-            return vim.fn.expand(
-              vim.fn.stdpath 'data' .. '/mason/packages/jdtls/config_win'
-            )
-          end
-
-          return ''
-        end
-
-        local get_data_path = function()
-          return vim.fn.expand(
-            (vim.fn.stdpath 'cache') .. '/jdtls/workspace/' .. project_name
-          )
-        end
-
-        vim.api.nvim_create_autocmd('FileType', {
-          pattern = 'java',
-          callback = function()
-            require('jdtls').start_or_attach {
-              capabilities = capabilities,
+        local set_nvim_jdtls_config = function(config)
+          if is_win() then
+            nvim_jdtls_config = vim.tbl_deep_extend('force', {}, {
+              capabilities = {},
               name = 'jdtls',
               cmd = {
-                'java',
+                vim.fn.expand '~/scoop/apps/openjdk/current/bin/java',
                 '-Declipse.application=org.eclipse.jdt.ls.core.id1',
                 '-Dosgi.bundles.defaultStartLevel=4',
                 '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -118,20 +89,61 @@ return {
                 '--add-opens',
                 'java.base/java.lang=ALL-UNNAMED',
                 '-jar',
-                get_equinox_launcher_path(),
+                vim.fn.expand(
+                  (vim.fn.stdpath 'data')
+                    .. '/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'
+                ),
+
                 '-configuration',
-                get_config_path(),
+                vim.fn.expand(
+                  (vim.fn.stdpath 'data') .. '/mason/packages/jdtls/config_win'
+                ),
                 '-data',
-                get_data_path(),
+                vim.fn.expand(
+                  (vim.fn.stdpath 'cache')
+                    .. '/jdtls/workspace/'
+                    .. project_name
+                ),
               },
               root_dir = root_dir,
               settings = {
-                java = {},
+                java = {
+                  configuration = {
+                    updateBuildConfiguration = 'automatic',
+                    runtimes = {
+                      {
+                        name = 'JavaSE-17',
+                        path = vim.fn.expand '~/scoop/apps/openjdk17/current',
+                      },
+                      {
+                        name = 'JavaSE-21',
+                        path = vim.fn.expand '~/scoop/apps/openjdk21/current',
+                      },
+                      {
+                        name = 'JavaSE-25',
+                        path = vim.fn.expand '~/scoop/apps/openjdk/current',
+                      },
+                    },
+                  },
+                },
               },
               init_options = {
-                bundles = bundles,
+                bundles = vim.fn.expand(
+                  (vim.fn.stdpath 'data')
+                    .. '/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar'
+                ),
               },
-            }
+            }, config)
+
+            return
+          end
+        end
+
+        vim.api.nvim_create_autocmd('FileType', {
+          pattern = 'java',
+          callback = function()
+            set_nvim_jdtls_config { capabilities = capabilities }
+            require('jdtls').start_or_attach(nvim_jdtls_config)
           end,
         })
 
